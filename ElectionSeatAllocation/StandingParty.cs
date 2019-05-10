6fe5c11e55088472f7463f25200467917fbe6aab
Jackie Pearce <jackiepearce83@hotmail.com>
2019-05-10 10:05:07 +0100

    StandingParty class written

---
+++ ElectionSeatAllocation/StandingParty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectionSeatAllocation
+{
+    public class StandingParty
+    {
+        public string Name { get; set; }
+        public int Votes { get; set; }
+
+        public int Seats { get; set; }
+
+        public decimal Quotient { get; private set; }
+        
+        
+        public void SetQuotient()
+        {
+            Quotient = Votes / (Seats + 1);
+        }
+
+
+
+    }
+}

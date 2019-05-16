caa9af34e24b15a6b760726ec197bcb2feb97dbe
Jackie Pearce <jackiepearce83@hotmail.com>
2019-05-16 15:33:39 +0100

    Refactor - election data and data input methods in ne ElectionData class

---
+++ ElectionSeatAllocation/ElectionData.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectionSeatAllocation
+{
+    public class ElectionData
+    {
+        public int NumberSeatsToBeAllocated;
+
+        public int NumberPartiesStanding;
+
+        public List<StandingParty> StandingParties;
+
+        void RetrieveNumberOfSeats()
+        {
+            var instruction = $"Enter number of seats to be allocated: ";
+
+            NumberSeatsToBeAllocated = GetInputNumeric(instruction);
+        }
+
+        private int GetInputNumeric(string instruction)
+        {
+            Console.WriteLine(instruction);
+            var input = Console.ReadLine();
+            try
+            {
+                return int.Parse(input);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid entry. Please use only digits 0-9");
+                return GetInputNumeric(instruction);
+            }
+        }
+
+        void RetrieveNumberOfParties()
+        {
+            var instruction = $"Enter number of parties standing: ";
+
+            NumberPartiesStanding = GetInputNumeric(instruction);
+
+        }
+
+        void RetrieveName(StandingParty party)
+        {
+            Console.WriteLine("Enter party name: ");
+            party.Name = Console.ReadLine();
+        }
+
+        void RetrieveNumberOfVotes(StandingParty party)
+        {
+            var instruction = $"Enter number of votes for {party.Name}: ";
+            party.Votes = GetInputNumeric(instruction);
+
+        }
+
+        void SetPartyData()
+        {
+            RetrieveNumberOfSeats();
+
+            StandingParties = new List<StandingParty>();
+
+            RetrieveNumberOfParties();
+
+            for (int i = 0; i < NumberPartiesStanding; i++)
+            {
+                var party = new StandingParty();
+                Console.WriteLine();
+                RetrieveName(party);
+                RetrieveNumberOfVotes(party);
+                StandingParties.Add(party);
+            }
+        }
+
+        public ElectionData()
+        {
+            SetPartyData();
+        }
+    }
+}
--- ElectionSeatAllocation/Program.cs
+++ ElectionSeatAllocation/Program.cs
@@ -6,96 +6,25 @@ namespace ElectionSeatAllocation
 {
     class Program
     {
-        static int NumberSeatsToBeAllocated;
-        
-        static int NumberPartiesStanding;
-
-        static List<StandingParty> StandingParties;
-
-        static void RetrieveNOfSeats()
-        {
-            Console.WriteLine($"Enter number of seats to be allocated: ");
-            var nOfSeats = Console.ReadLine();
-            try
-            {
-                NumberSeatsToBeAllocated = int.Parse(nOfSeats);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Invalid entry. Please use only digits 0-9");
-                RetrieveNOfSeats();
-            }
-        }
-
-        static void SetPartyData()
-        {
-            StandingParties = new List<StandingParty>();
-
-            RetrieveNOfParties();
-
-            for (int i = 0; i < NumberPartiesStanding; i++)
-            {
-                var party = new StandingParty();
-                Console.WriteLine();
-                RetrieveName(party);
-                RetrieveNOfVotes(party);
-                StandingParties.Add(party);
-            }
-        }
-
-        static void RetrieveNOfParties()
-        {
-            Console.WriteLine($"Enter number of parties standing: ");
-            var nOfParties = Console.ReadLine();
-            try
-            {
-                NumberPartiesStanding = int.Parse(nOfParties);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Invalid entry. Please use only digits 0-9");
-                RetrieveNOfParties();
-            }
-        }
-
-        private static void RetrieveNOfVotes(StandingParty party)
-        {
-            Console.WriteLine($"Enter number of votes for {party.Name}: ");
-            var voteEntry = Console.ReadLine();
-            try
-            {
-                party.Votes = int.Parse(voteEntry);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Invalid entry. Please use only digits 0-9");
-                RetrieveNOfVotes(party);
-            }
-        }
-
-        private static void RetrieveName(StandingParty party)
-        {
-            Console.WriteLine("Enter party name: ");
-            party.Name = Console.ReadLine();
-        }
-
         static void Main(string[] args)
         {
-            RetrieveNOfSeats();
-            SetPartyData();
-            var seatAllocator = new SeatAllocator(StandingParties);
+            var electionData = new ElectionData();
 
-            for (int i = 0; i < NumberSeatsToBeAllocated; i++)
+            var standingParties = electionData.StandingParties;
+
+            var seatAllocator = new SeatAllocator(standingParties);
+
+            for (int i = 0; i < electionData.NumberSeatsToBeAllocated; i++)
             {
                 seatAllocator.AllocateSeat();
             }
 
-            StandingParties = StandingParties.OrderByDescending(e => e.Seats).ToList();
+            standingParties = standingParties.OrderByDescending(e => e.Seats).ToList();
 
             Console.WriteLine();
             Console.WriteLine("Results");
 
-            foreach (var party in StandingParties)
+            foreach (var party in standingParties)
             {
                 Console.WriteLine($"{party.Name} {party.Seats} seats");
             }
